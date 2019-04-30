59c0f5068c7e40153faffa0d4051fa9a923aa23b
mctcst1 <bestmail.mct@gmail.com>
2019-05-01 00:46:09 +0430

    4

--- app1/admin.py
+++ app1/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from . import models
 
-
+# admin.site.register(models.Test, Optinal_class_for change_display) # or
 @admin.register(models.Test)
-class Teset(admin.ModelAdmin):
-    pass
+class Teste(admin.ModelAdmin):
+    list_display = ('name', 'degree')
+    search_fields = ('name', 'degree')
+    list_filter = ('name', 'degree')
+    date_hierarchy = 'date'
+    ordering = ('-degree', )
--- app1/models.py
+++ app1/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Test(models.Model):
     name = models.CharField(max_length=50)
     degree = models.IntegerField(8)
+    date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return '%s: %s' % (self.name, self.degree)
