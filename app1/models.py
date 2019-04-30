ce83f30f2b7344b723c4ec3bcf44111668975eaf
mctcst1 <bestmail.mct@gmail.com>
2019-04-30 23:19:39 +0430

    3

--- app1/models.py
+++ app1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Test(models.Model):
     name = models.CharField(max_length=50)
-    degree = models.IntegerField(10)
+    degree = models.IntegerField(8)
 
     def __str__(self):
         return '%s: %s' % (self.name, self.degree)
--- app1/views.py
+++ app1/views.py
@@ -4,4 +4,5 @@ from . import models
 
 def test(request):
     name = models.Test.objects.get(id=1)
-    return render(request, 'index.html', {'name': name, 'degree': 3.5})
+    data = models.Test.objects.all()
+    return render(request, 'index.html', {'name': name, 'degree': 3.5, 'data': data})
